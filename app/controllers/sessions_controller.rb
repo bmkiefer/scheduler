1ef674cfbbfb4e667597faecd1f86d3b585ad51a
bkiefer88 <brandon-kiefer@uiowa.edu>
2013-04-08 06:11:42 -0700

    login page built

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+     user = User.find_by_username(params[:session][:username].downcase)
+     if user && user.authenticate(params[:session][:password])
+       sign_in user
+       redirect_to user
+     else
+     flash.now[:error] = 'Invalid username/password combination'
      render 'new'
+     end
   end
 
   def destroy
