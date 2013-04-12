7442b0c938a142b8ce8af4b114a60993b2ef387c
nhalbmaier <saasbook@saasbook.(none)>
2013-04-11 19:21:06 -0700

    Editing

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,19 +1,3 @@
 class SubmissionsController < ApplicationController
 
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @submission = Submission.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-
-    @all_ratings = Submission.all_ratings
-   
-  end
-
-  def new
-    # default: render 'new' template
-  end
-
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,8 +1,4 @@
 class Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :level
-
-  def self.all_ratings
-    %w(1 2 3 4 5)
-  end
 end
