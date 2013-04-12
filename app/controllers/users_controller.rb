20bcef9167a379d4ea24fcca3f15bb0c09e24417
kdornbier <kaitlyn-dornbier@uiowa.edu>
2013-04-11 17:46:37 -0700

    Table inserted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @user = User.find(id) # look up movie by unique ID
+    @users_all = User.all
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
     @users = User.all
   end
+  
 
   def new
     # default: render 'new' template
