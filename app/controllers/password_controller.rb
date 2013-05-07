5061c54008c2d4e9d258e5da2f0dc1d2195c5166
kdornbier <kaitlyn-dornbier@uiowa.edu>
2013-05-06 22:22:43 -0700

    Website does not sign out on password update

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -5,19 +5,17 @@ class PasswordController < ApplicationController
    end
    
    def update
-      #@user = User.find params[:id]
-      #@user2 = User.where(:password => 'old_password')
-      #@user.update_attributes!(params[:user])
-      #flash[:notice] = "Password for #{@user.username} has been changed."
-      
-      #if  @user2 == @user 
+      @user = User.find params[:id]
+
+      if @user.password == params[:old_password] then
+         @user.password = params[:user][:new_password]
+         @user.password_confirmation = params[:user][:new_password_confirmation] 
          @user.update_attributes!(params[:user])
-         #@user.password = params[:password]
-         #@user.password_confirmation = params[:password_confirmation] 
          flash[:notice] = "Password for #{@user.username} has been changed."
-     #else
-     #    flash.now[:error] = "Invalid password authentication. Password unchanged."
-     #end
+         sign_in @user
+      else
+         flash.now[:error] = "Invalid password authentication. Password unchanged."
+     end
      redirect_to user_path(@user)
    end
 
