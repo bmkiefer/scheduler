fd7d7ea9548c904c3ac31fee4580f5a0a618a3e7
bkiefer88 <brandon-kiefer@uiowa.edu>
2013-07-17 13:13:24 -0700

    seed completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,43 @@
 
 user_t = [
 	  {:username => 'k889722', :email => 'mist@gmail.com', :password => 'yeahmaybe',:password_confirmation => 'yeahmaybe', :role => 'user', :position => 'Project Manager', :department => 'Information Security', :Leader=> 'y908706'},
-          {:username => 'p099044', :email => 'jmk@uiowa.edu', :password => 'password1',:password_confirmation => 'password1', :role => 'user', :position => 'System Analyst', :department => 'RIS', :Leader=> 'y908706'},
+          {:username => 'p099044', :email => 'jmk@uiowa.edu', :password => 'password1',:password_confirmation => 'password1', :role => 'user', :position => 'Business Analyst', :department => 'RIS', :Leader=> 'y908706'},
           {:username => 'h789987', :email => 'james@hotmail.com', :password => 'password2',:password_confirmation => 'password2', :role => 'user', :position => 'Business Anaylst', :department => 'USIS', :Leader=> 'y908706'},
           {:username => 'n097654', :email => 'krogers@live.com', :password => 'password3',:password_confirmation => 'password3', :role => 'user', :position => 'Lead Application Analyst', :department => 'Life', :Leader=> 'y908706'},
           {:username => 'r456786', :email => 'tina@gmail.com', :password => 'password4',:password_confirmation => 'password4', :role => 'admin', :position => 'admin', :department => 'admin', :Leader=> 'admin'}
-          
           ]
 
 venue_t = [
 	  {:location => 'Wells Fargo Arena',:room => 'Suite 32, Section 117', :tickets => 12, :extra_max => 8},
-          {:location => 'Principal Park',:room => 'John', :tickets => 10, :extra_max => 10},
-          {:location => 'Principal Park',:room => 'James', :tickets => 10, :extra_max => 10}
+          {:location => 'Principal Park',:room => 'SkyBox 309', :tickets => 10, :extra_max => 10},
+          {:location => 'Principal Park',:room => 'SkyBox 310', :tickets => 10, :extra_max => 10}
           ]
 
-game_t = [
-	  {:location => 'Wells Fargo Arena',:room => 'Suite 32, Section 117', :tickets => 12, :extra_max => 8},
-          {:location => 'Principal Park',:room => 'John', :tickets => 10, :extra_max => 10},
-          {:location => 'Principal Park',:room => 'James', :tickets => 10, :extra_max => 10}
+gameicubs_t = [
+	  {:game_time => DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Cubs', :opposing_team => },
+          {:game_time => DateTime.strptime("09/15/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Suite 32, Section 117', :opposing_team => 12},
+          {:game_time => DateTime.strptime("09/16/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Suite 32, Section 117', :opposing_team => 12},
+          {:game_time => DateTime.strptime("09/17/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Suite 32, Section 117', :opposing_team => 12},
+          {:game_time => DateTime.strptime("09/18/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Suite 32, Section 117', :opposing_team => 12},
+          {:game_time => DateTime.strptime("09/19/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Suite 32, Section 117', :opposing_team => 12}
+          ]
+
+gamebarnstormers_t = [
+	  {:game_time => DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Barn Stormers', :opposing_team => 'Arizona Rattlers'},
+          {:game_time => DateTime.strptime("09/15/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Barn Stormers', :opposing_team => 'Arizona Rattlers'},
+          {:game_time => DateTime.strptime("09/16/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Barn Stormers', :opposing_team => 'Arizona Rattlers'},
+          {:game_time => DateTime.strptime("09/17/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Barn Stormers', :opposing_team => 'Arizona Rattlers'},
+          {:game_time => DateTime.strptime("09/18/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Barn Stormers', :opposing_team => 'Arizona Rattlers'},
+          {:game_time => DateTime.strptime("09/19/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Barn Stormers', :opposing_team => 'Arizona Rattlers'}
+          ]
+
+gameenergy_t = [
+	  {:game_time => DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Energy', :opposing_team => 'Texas Legends'},
+          {:game_time => DateTime.strptime("09/15/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Energy', :opposing_team => 'Texas Legends'},
+          {:game_time => DateTime.strptime("09/16/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Energy', :opposing_team => 'Texas Legends'},
+          {:game_time => DateTime.strptime("09/17/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Energy', :opposing_team => 'Texas Legends'},
+          {:game_time => DateTime.strptime("09/18/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Energy', :opposing_team => 'Texas Legends'},
+          {:game_time => DateTime.strptime("09/19/2009 8:00", "%m/%d/%Y %H:%M"),:team => 'Iowa Energy', :opposing_team => 'Texas Legends'}
           ]
 
 user_t.each do |user|
@@ -35,143 +55,46 @@ venue_t.each do |venue|
    Venue.create!(venue)
 end
 
-level_t.each do |level|
-  Level.create!(level)
+gameicubs_t.each do |game|
+  Game.create!(game)
 end
 
+gamebarnstormers_t.each do |game|
+  Game.create!(game)
+end
 
-question_t = [
-		'What did you learn in this mission?',
-		'What did you not quite get?',
-		'How smart do you feel now?'
-	     ]	
+gameenergy_t.each do |game|
+  Game.create!(game)
+end
 
+	
 all_levels = Level.all
 all_users = User.all
 
-@current_level = Level.find_by_level_name('Level 1')
+@transaction = Transactionlevel.find_by_level_id_and_user_id(@level.id,@user.id)
 
-  mission1_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
+@current_venue = Venue.find_by_location_and_room('Principal Park','SkyBox 309')
+@icubs = Game.where(:team => 'Iowa Cubs')
 
-@current_level = Level.find_by_level_name('Level 2')
-
-  mission2_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-@current_level = Level.find_by_level_name('Level 3')
-
-  mission3_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-@current_level = Level.find_by_level_name('Level 4')
-
-  mission4_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-
-@current_level = Level.find_by_level_name('Level 5')
-
-  mission5_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-@current_level = Level.find_by_level_name('Level 6')
-
-  mission6_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-@current_level = Level.find_by_level_name('Level 7')
-
-  mission7_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-@current_level = Level.find_by_level_name('Level 8')
-
-  mission8_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-@current_level = Level.find_by_level_name('Level 9')
-
-  mission9_t.each do |mission|
-    mission[:level_id] = @current_level.id
-    @mission = Mission.create!(mission)
-    @updatelevel = Level.find(@mission.level_id)
-    @updatelevel.points = @updatelevel.points + @mission.points
-    @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  end
-
-#all_levels.each do |level|
-  #mission_t.each do |mission|
- #   mission[:level_id] = level.id
-  #  @mission = Mission.create!(mission)
-  #  @updatelevel = Level.find(@mission.level_id)
- #   @updatelevel.points = @updatelevel.points + @mission.points
- #   @updatelevel.update_attributes!(:level_name => @updatelevel.level_name, :points => @updatelevel.points, :activity => @updatelevel.activity)
-  #end
-#end
-
-all_users.each do |user|
-  all_levels.each do |level|
-    Mission.where(:level_id => level.id).each do |mission_obj|
-      Transactionmission.create!(:complete_flag => "Not Complete",:user_id => user.id,:level_id => level.id, :mission_id => mission_obj.id)
-    end	 
-  end
+@icubs.each do |game|
+  Reservation.create!(:game_id => game.id, :venue_id => @current_venue.id, :status => 'Open')
 end
 
-all_users.each do |user|
-  all_levels.each do |level|
-    Transactionlevel.create(:complete_flag => "Not Complete",:user_id => user.id,:level_id => level.id)	 
-  end
+@current_venue = Venue.find_by_location_and_room('Principal Park','SkyBox 310')
+
+@icubs.each do |game|
+  Reservation.create!(:game_id => game.id, :venue_id => @current_venue.id, :status => 'Open')
 end
 
-Level.all.each do |level|
-  Mission.where(:level_id => level.id).each do |mission|
-    Submission.create(:mission_id => mission.id, :level_id => level.id )
-  end
+@current_venue = Venue.find_by_location_and_room('Wells Fargo Arena','Suite 32, Section 117')
+@barnstormers = Game.where(:team => 'Iowa Barn Stormers')
+
+@icubs.each do |game|
+  Reservation.create!(:game_id => game.id, :venue_id => @current_venue.id, :status => 'Open')
 end
 
-Submission.all.each do |submission|
-  question_t.each do |question|
-     Question.create(:submission_id => submission.id, :question => question)
-  end
+@energy = Game.where(:team => 'Iowa Energy')
+
+@icubs.each do |game|
+  Reservation.create!(:game_id => game.id, :venue_id => @current_venue.id, :status => 'Open')
 end	
